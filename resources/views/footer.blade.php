eed5e674f60e39a9734de95f5cab9c4f80036bfe
SolDok <mrdazko@gmail.com>
2023-11-30 15:40:01 +0700

    Remake auth system

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,2 +1,4 @@
 </body>
+<script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/script.js') }}"></script>
 </html>
